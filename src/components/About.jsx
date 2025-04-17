ec8eab06ff40aecbcf8cd921f7ba9c10cd1aab98
SkylaarN <157369173+SkylaarN@users.noreply.github.com>
2025-04-17 23:03:20 +0100

    initial

---
+++ src/components/About.jsx
@@ -0,0 +1,70 @@
+import React from "react";
+import Tilt from "react-tilt";
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { services } from "../constants";
+import { SectionWrapper } from "../hoc";
+import { fadeIn, textVariant } from "../utils/motion";
+
+const ServiceCard = ({ index, title, icon }) => (
+  <Tilt className='xs:w-[250px] w-full'>
+    <motion.div
+      variants={fadeIn("right", "spring", index * 0.5, 0.75)}
+      className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
+    >
+      <div
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
+      >
+        <img
+          src={icon}
+          alt='web-development'
+          className='w-16 h-16 object-contain'
+        />
+
+        <h3 className='text-white text-[20px] font-bold text-center'>
+          {title}
+        </h3>
+      </div>
+    </motion.div>
+  </Tilt>
+);
+
+const About = () => {
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
+
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
+      >
+      I’m a tech enthusiast with a background that bridges both hardware and software. My journey began in electronics engineering, where I learned how systems function at their core. Over time, that curiosity evolved into a passion for software development and design.
+
+      I’ve completed a full-time systems development program at WeThinkCode_, where I built a strong foundation in Python, Java, SQL, web development, mobile app development with Flutter, and test-driven development. I also expanded my skillset into data analytics through an IBM course, where I worked with tools like Tableau and Excel to turn raw data into actionable insights.
+
+      As a self-driven learner, I’ve also picked up design tools like Figma, Canva, and Adobe 
+
+      Photoshop to bring visual polish to the products I build. Whether I’m coding, designing, or analyzing data, I love creating meaningful tech experiences that make an impact.
+
+      I’m currently open to collaborations, internships, or junior roles where I can continue to grow, contribute, and solve real-world problems through technology.
+      </motion.p>
+
+      <div className='mt-20 flex flex-wrap gap-10'>
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default SectionWrapper(About, "about");
